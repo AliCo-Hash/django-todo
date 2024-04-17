c5e90f2cee58f8db1c59765c5ca393792c0629af
AliCo-Hash <ali.cocelli@hotmail.co.uk>
2024-04-17 09:42:29 +0100

    changed to en-uk and added object representation to model

--- todo/models.py
+++ todo/models.py
@@ -4,3 +4,6 @@ class ToDoNote(models.Model):
   note = models.CharField(max_length=200)
   completed = models.BooleanField(default=False)
   created_date = models.DateField(auto_now_add=True)
+
+  def __str__(self):
+    return self.note
