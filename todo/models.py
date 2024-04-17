b9f1ef01336925998a65bc936f654aa2aaacc5da
AliCo-Hash <ali.cocelli@hotmail.co.uk>
2024-04-17 09:11:26 +0100

    todo app model created

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class ToDoNote(models.Model):
+  note = models.CharField(max_length=200)
+  completed = models.BooleanField(default=False)
+  created_date = models.DateField(auto_now_add=True)
